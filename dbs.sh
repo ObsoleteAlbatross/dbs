eb641b0c60fb1af24be941b050612e7b2377248f
Charles Yan <charles5850@gmail.com>
2020-09-05 18:19:54 +0000

    Git clone user fix

--- dbs.sh
+++ dbs.sh
@@ -24,7 +24,7 @@ installyay() {
     echo "Installing 'yay' (aur helper)"
     mkdir -p "/home/$user/src"
     cd "/home/$user/src"
-    git clone "https://aur.archlinux.org/yay.git"
+    sudo -u "$name" git clone "https://aur.archlinux.org/yay.git"
     cd yay
     sudo -u "$name" makepkg --noconfirm -si > /dev/null 2>&1
   }
@@ -32,7 +32,7 @@ installyay() {
 
 
 installpkgwrap() {
-  "Installing '$1' ($n of $total)"
+  echo "Installing '$1' ($n of $total)"
   installpkg "$1"
 }
 
@@ -50,7 +50,7 @@ installcsv() {
 
 installdots() {
   # install 'yadm'
-  git clone "https://github.com/TheLocehiliosan/yadm.git" "/home/$user/src/yadm"
+  sudo -u "$name" git clone "https://github.com/TheLocehiliosan/yadm.git" "/home/$user/src/yadm"
   [ -d "/home/$user/.local/bin" ] || mkdir -p "/home/$user/.local/bin"
   ln -s "/home/$user/src/yadm/yadm" "/home/$user/.local/bin"
 
@@ -88,8 +88,8 @@ chsh -s /bin/zsh $name >/dev/null 2>&1
 sudo -u "$name" mkdir -p "/home/$name/.cache/"
 sudo -u "$name" touch "/home/$name/.cache/shell_history"
 mkdir -p "/home/$user/.config/zsh/plugins"
-git clone "https://github.com/zdharma/fast-syntax-highlighting" "/home/$user/.config/zsh/plugins/fsh"
-git clone "https://github.com/zsh-users/zsh-completions.git" "/home/$user/.config/zsh/plugins/zsh-completions"
+sudo -u "$name" git clone "https://github.com/zdharma/fast-syntax-highlighting" "/home/$user/.config/zsh/plugins/fsh"
+sudo -u "$name" git clone "https://github.com/zsh-users/zsh-completions.git" "/home/$user/.config/zsh/plugins/zsh-completions"
 rm -f "/home/$user/.zcompdump"
 compinit
 
@@ -100,15 +100,15 @@ if command -v nvim >/dev/null 2>&1; then
 fi
 
 # Install universal ctags
-git clone "https://github.com/universal-ctags/ctags" "/home/$user/src"
+sudo -u "$name" git clone "https://github.com/universal-ctags/ctags" "/home/$user/src"
 cd "/home/$user/src/ctags"
-./autogen.sh
-./configure --prefix="/home/$user/.local/bin"
+sudo -u "$name" /home/$user/src/ctags/autogen.sh
+sudo -u "$name" /home/$user/src/ctags/configure --prefix="/home/$user/.local/bin"
 sudo -u "$name" make
 sudo -u "$name" make install
 
 # Install st
-git clone "https://github.com/LukeSmithxyz/st" "/home/$user/src"
+sudo -u "$name" git clone "https://github.com/LukeSmithxyz/st" "/home/$user/src"
 cd "/home/$user/src/st"
 make install
 ln -s "/home/$user/src/st/st" "/home/$user/.local/bin/sh"
