461520516b35800894a0ccedd961afef2612ba87
Charles Yan <charles5850@gmail.com>
2020-09-05 20:23:40 +0000

    Wallpaper

--- dbs.sh
+++ dbs.sh
@@ -91,7 +91,7 @@ sudo -u "$name" mkdir -p "/home/$name/.config/zsh/plugins"
 sudo -u "$name" git clone "https://github.com/zdharma/fast-syntax-highlighting" "/home/$name/.config/zsh/plugins/fsh" > /dev/null 2>&1
 sudo -u "$name" git clone "https://github.com/zsh-users/zsh-completions.git" "/home/$name/.config/zsh/plugins/zsh-completions" > /dev/null 2>&1
 rm -f "/home/$name/.zcompdump"
-compinit
+compinit < /dev/null 2>&1
 
 # Setup nvim
 sudo -u "$name" sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
@@ -104,16 +104,24 @@ fi
 echo "Installing universal ctags"
 sudo -u "$name" git clone "https://github.com/universal-ctags/ctags" "/home/$name/src/ctags" > /dev/null 2>&1
 cd "/home/$name/src/ctags"
-sudo -u "$name" /home/$name/src/ctags/autogen.sh
-sudo -u "$name" /home/$name/src/ctags/configure --prefix="/home/$name/.local/bin"
-sudo -u "$name" make
-sudo -u "$name" make install
+sudo -u "$name" /home/$name/src/ctags/autogen.sh < /dev/null 2>&1
+sudo -u "$name" /home/$name/src/ctags/configure --prefix="/home/$name/.local/bin" < /dev/null 2>&1
+sudo -u "$name" make < /dev/null 2>&1
+sudo -u "$name" make install < /dev/null 2>&1
 
 # Install st
 echo "Installing st"
 sudo -u "$name" git clone "https://github.com/LukeSmithxyz/st" "/home/$name/src/st" > /dev/null 2>&1
 cd "/home/$name/src/st"
-make install
+make install < /dev/null 2>&1
 ln -s "/home/$name/src/st/st" "/home/$name/.local/bin/sh"
+
+# Install pfetch
+echo "Installing pfetch"
+sudo -u "$name" git clone "https://github.com/dylanaraps/pfetch" "/home/$name/src/pfetch" > /dev/null 2>&1
+ln -s "/home/$name/src/pfetch" "/home/$name/.local/bin/pfetch"
+
+# Setup bg
+sudo -u "$name" "/home/$name/pictures/wallpapers/310e488.png"
 
 echo "Done!"
